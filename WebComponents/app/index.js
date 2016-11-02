f6a129a222ce88f058e6c8c32ecf995fd37cdc73
MeePwn <maybewaityou@gmail.com>
2016-11-02 09:34:17 +0800

    first commit

--- WebComponents/app/index.js
+++ WebComponents/app/index.js
@@ -4,7 +4,8 @@ import TestExample from './example/testExample/TestExample';
 import TestMobileFirstExample from './example/testExample/TestMobileFirstExample';
 import RouterExample from './example/routerExample/routerExample';
 import SlickExample from './example/slickExample/SlickExample';
-import App from './App';
+// import App from './App';
+import App from './homePage/App';
 
 import { Provider } from 'react-redux';
 import configureStore from './framework/store/Store';
