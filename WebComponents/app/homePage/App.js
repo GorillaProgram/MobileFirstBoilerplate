6d7ba7180213000bb14d59157140570a8b2a7629
MeePwn <maybewaityou@gmail.com>
2016-11-02 11:16:17 +0800

    first commit

---
+++ WebComponents/app/homePage/App.js
@@ -0,0 +1,24 @@
+/**
+ * Created by MeePwn
+ * https://github.com/maybewaityou
+ */
+import React, { Component } from 'react';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+
+    }
+
+    render() {
+        return (
+            <div>
+            </div>
+        );
+    }
+}
+
+
+
+export default App;
