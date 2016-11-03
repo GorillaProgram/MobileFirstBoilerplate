e7c39f17ca374d4442b7f82eeb6348e03146f1b4
MeePwn <maybewaityou@gmail.com>
2016-11-03 11:35:28 +0800

    first commit

--- WebComponents/app/main/manager/UIManager.js
+++ WebComponents/app/main/manager/UIManager.js
@@ -5,10 +5,12 @@
 import DebugUtility from '../utilities/function/DebugUtility';
 import Dimensions from '../utilities/userInterface/Dimensions';
 import ImageCache from '../utilities/userInterface/ImageCache';
+import JQMUtility from '../utilities/userInterface/JQMUtility';
 
 const UIManager = {
     ...Dimensions,
-    ...ImageCache
+    ...ImageCache,
+    ...JQMUtility
 };
 
 export default UIManager;
--- WebComponents/app/test/Test00.js
+++ WebComponents/app/test/Test00.js
@@ -16,14 +16,23 @@ class Test00 extends Component {
 
     render() {
         return (
-            <div onClick={() => {
+            <div style={styles.container} onClick={() => {
                 // this.context.router.push('test01');
-                UIManager.cacheFile(['http://10.240.90.212:7001/padServer/padimages/1.png']);
+                // UIManager.cacheFile(['http://10.240.90.212:7001/padServer/padimages/1.png']);
+                UIManager.showLoader();
             }}>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                <button className="ui-btn" data-role="button">button</button>
                 {this.props.children}
             </div>
         );
+    }
+}
+
+const styles = {
+    container: {
+        position: 'fixed',
+        top: 0, left: 0, bottom: 0, right: 0,
+        backgroundColor: 'white'
     }
 }
 
