0cb812761b92af89d5a567068b9f00b8b9e7490c
MeePwn <maybewaityou@gmail.com>
2016-11-04 18:57:07 +0800

    first commit

--- WebComponents/app/main/constant/Just.js
+++ WebComponents/app/main/constant/Just.js
@@ -1,3 +1,4 @@
+import LifeCircle from '../lifeCircle/LifeCircle';
 import DataManager from '../manager/DataManager';
 import FunctionManager from '../manager/FunctionManager';
 import UIManager from '../manager/UIManager';
@@ -5,6 +6,7 @@ import PeripheralManager from '../manager/PeripheralManager';
 import TaskManager from '../manager/TaskManager';
 
 const Just = {
+    ...LifeCircle,
     ...DataManager,
     ...FunctionManager,
     ...UIManager,
