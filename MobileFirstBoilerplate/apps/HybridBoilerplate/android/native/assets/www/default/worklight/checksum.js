949cad87e749012603604f9c7b91903c345e6d9e
MeePwn <maybewaityou@gmail.com>
2016-11-07 18:36:41 +0800

    first commit

--- MobileFirstBoilerplate/apps/HybridBoilerplate/android/native/assets/www/default/worklight/checksum.js
+++ MobileFirstBoilerplate/apps/HybridBoilerplate/android/native/assets/www/default/worklight/checksum.js
@@ -1,2 +1,2 @@
-var WL_CHECKSUM = {"date":1478506827795,"machine":"192.168.1.109","checksum":2589653464};
-/* Date: Mon Nov 07 16:20:27 CST 2016 */
+var WL_CHECKSUM = {"date":1478513967188,"machine":"192.168.1.109","checksum":3996900698};
+/* Date: Mon Nov 07 18:19:27 CST 2016 */
