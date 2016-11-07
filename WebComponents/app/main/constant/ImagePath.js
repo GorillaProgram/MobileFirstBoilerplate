9eea16ef7b2993dc38f8cd37cc50673ddd779bef
MeePwn <maybewaityou@gmail.com>
2016-11-07 16:06:35 +0800

    first commit

--- WebComponents/app/main/constant/ImagePath.js
+++ WebComponents/app/main/constant/ImagePath.js
@@ -2,23 +2,32 @@
  * Created by MeePwn
  * https://github.com/maybewaityou
  */
+
+const imagePath = `./static/imgs/`;
+
 const ImagePath = {
 
-    homeTabActiveIcon: './static/imgs/homeTabActiveIcon.png',
-    homeTabIcon: './static/imgs/homeTabIcon.png',
-    productionTabActiveIcon: './static/imgs/productionTabActiveIcon.png',
-    productionTabIcon: './static/imgs/productionTabIcon.png',
-    businessManagementTabActiveIcon: './static/imgs/businessManagementTabActiveIcon.png',
-    businessManagementTabIcon: './static/imgs/businessManagementTabIcon.png',
-    commonToolsTabActiveIcon: './static/imgs/commonToolsTabActiveIcon.png',
-    commonToolsTabIcon: './static/imgs/commonToolsTabIcon.png',
-    logo: './static/imgs/logo.png',
-    defaultAdImage: './static/imgs/ad.png',
-    progressbar: './static/imgs/progressbar.png',
-    prevArrow: './static/imgs/prevArrow.png',
-    nextArrow: './static/imgs/nextArrow.png',
-    noticeContentImage: './static/imgs/noticeContentImage.png',
-    
+    homeTabActiveIcon: `${imagePath}homeTabActiveIcon.png`,
+    homeTabIcon: `${imagePath}homeTabIcon.png`,
+    productionTabActiveIcon: `${imagePath}productionTabActiveIcon.png`,
+    productionTabIcon: `${imagePath}productionTabIcon.png`,
+    businessManagementTabActiveIcon: `${imagePath}businessManagementTabActiveIcon.png`,
+    businessManagementTabIcon: `${imagePath}businessManagementTabIcon.png`,
+    commonToolsTabActiveIcon: `${imagePath}commonToolsTabActiveIcon.png`,
+    commonToolsTabIcon: `${imagePath}commonToolsTabIcon.png`,
+    logo: `${imagePath}logo.png`,
+    defaultAdImage: `${imagePath}ad.png`,
+    progressbar: `${imagePath}progressbar.png`,
+    prevArrow: `${imagePath}prevArrow.png`,
+    nextArrow: `${imagePath}nextArrow.png`,
+    noticeContentImage: `${imagePath}noticeContentImage.png`,
+    homeOpenCard: `${imagePath}cardOpenIcon.png`,
+    homeContract: `${imagePath}contractIcon.png`,
+    homeManageMoney: `${imagePath}manageMoneyIcon.png`,
+    homeFund: `${imagePath}fundIcon.png`,
+    homeBenifit: `${imagePath}benifitIcon.png`,
+    homeSearch: `${imagePath}searchIcon.png`,
+
 
 };
 
