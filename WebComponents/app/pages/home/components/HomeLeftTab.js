e5a49ff8a1b3fa0d5c239e15b831e6c8cbf5b0ba
MeePwn <maybewaityou@gmail.com>
2016-11-07 13:55:49 +0800

    first commit

--- WebComponents/app/pages/home/components/HomeLeftTab.js
+++ WebComponents/app/pages/home/components/HomeLeftTab.js
@@ -1,3 +1,7 @@
+/**
+ * Created by MeePwn
+ * https://github.com/maybewaityou
+ */
 import React, { Component } from 'react';
 
 class HomeLeftTab extends Component {
--- WebComponents/app/pages/home/components/slider/components/Content.js
+++ WebComponents/app/pages/home/components/slider/components/Content.js
@@ -1,3 +1,7 @@
+/**
+ * Created by MeePwn
+ * https://github.com/maybewaityou
+ */
 import React, { Component } from 'react';
 
 class Content extends Component {
