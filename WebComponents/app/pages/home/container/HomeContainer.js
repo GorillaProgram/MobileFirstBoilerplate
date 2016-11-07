b734d6739e8e4fe0736b0285aaaffc356a2d4cd8
MeePwn <maybewaityou@gmail.com>
2016-11-07 13:54:35 +0800

    first commit

--- WebComponents/app/pages/home/container/HomeContainer.js
+++ WebComponents/app/pages/home/container/HomeContainer.js
@@ -4,9 +4,7 @@
  */
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
-import ImagePath from '../../../main/constant/ImagePath';
 import HomeView from '../view/HomeView';
-import Title from '../../../main/components/view/Title';
 
 class HomeContainer extends Component {
 
