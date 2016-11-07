8087b4832492e4ae0e16a995ada2e0dce9ea4d83
MeePwn <maybewaityou@gmail.com>
2016-11-07 08:56:26 +0800

    first commit

--- WebComponents/app/pages/home/components/HomeLeftLoginBox.js
+++ WebComponents/app/pages/home/components/HomeLeftLoginBox.js
@@ -9,47 +9,45 @@ class HomeLeftLoginBox extends Component {
     }
 
     render() {
-        const loginedBox = (
-            <div style={styles.loginBox}>
-                <div style={styles.userInfoLayout}>
-                    <div style={styles.headImageLayout}>
-                        <img style={styles.userHeadImage} src="./static/imgs/defaultHeadImage.png" />
-                    </div>
-                    <div style={styles.userInfoDesLayout}>
-                        <label style={styles.userInfo}>欢迎您, </label>
-                        <label style={styles.userInfo}>{this.props.userName}</label>
-                    </div>
-                </div>
-                <div style={styles.counterInfoLayout}>
-                    <label style={styles.counterInfo}>
-                        网点号: {this.props.orgCode}
-                    </label>
-                    <label style={styles.counterInfo}>
-                        柜员号: {this.props.userID}
-                    </label>
-                </div>
-                <div style={styles.userSetting} onClick={this.props.handleSettingClick}>
-                    <img style={styles.userSettingImage} src="./static/imgs/userSetting.png"/>
-                </div>
-            </div>
-        );
-        const notLoginBox = (
-            <div style={styles.loginBox}>
-                <div style={styles.userInfoLayout} onClick={this.props.handleLoginClick}>
-                    <div style={styles.headImageLayout}>
-                        <img style={styles.headImage} src="./static/imgs/loginIcon.png" />
-                    </div>
-                    <div style={styles.userInfoDesLayout}>
-                        <label style={styles.userInfo}>点击登录</label>
-                    </div>
-                </div>
-                <div style={styles.userSetting}>
-                    <img style={styles.userSettingImage} src="./static/imgs/userSetting.png"/>
-                </div>
-            </div>
-        );
         return (
-            this.props.isLogin === true ? loginedBox : notLoginBox
+            this.props.model ? (
+                <div style={styles.loginBox}>
+                    <div style={styles.userInfoLayout}>
+                        <div style={styles.headImageLayout}>
+                            <img style={styles.userHeadImage} src="./static/imgs/defaultHeadImage.png" />
+                        </div>
+                        <div style={styles.userInfoDesLayout}>
+                            <label style={styles.userInfo}>欢迎您, </label>
+                            <label style={styles.userInfo}>{this.props.model.userName}</label>
+                        </div>
+                    </div>
+                    <div style={styles.counterInfoLayout}>
+                        <label style={styles.counterInfo}>
+                            网点号: {this.props.model.orgCode}
+                        </label>
+                        <label style={styles.counterInfo}>
+                            柜员号: {this.props.model.userID}
+                        </label>
+                    </div>
+                    <div style={styles.userSetting} onClick={this.props.handleSettingClick}>
+                        <img style={styles.userSettingImage} src="./static/imgs/userSetting.png"/>
+                    </div>
+                </div>
+            ) : (
+                <div style={styles.loginBox}>
+                    <div style={styles.userInfoLayout} onClick={this.props.handleLoginClick}>
+                        <div style={styles.headImageLayout}>
+                            <img style={styles.headImage} src="./static/imgs/loginIcon.png" />
+                        </div>
+                        <div style={styles.userInfoDesLayout}>
+                            <label style={styles.userInfo}>点击登录</label>
+                        </div>
+                    </div>
+                    <div style={styles.userSetting}>
+                        <img style={styles.userSettingImage} src="./static/imgs/userSetting.png"/>
+                    </div>
+                </div>
+            )
         );
     }
 }
