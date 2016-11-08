1715d0e72efc0f11452f149adb00ec4bb6550a51
MeePwn <maybewaityou@gmail.com>
2016-11-08 20:17:44 +0800

    first commit

--- WebComponents/app/pages/login/view/LoginView.js
+++ WebComponents/app/pages/login/view/LoginView.js
@@ -3,6 +3,7 @@
  * https://github.com/maybewaityou
  */
 import React, { Component } from 'react';
+import Constant from '../../../main/constant/Constant';
 import CommonStyles from '../../../main/constant/CommonStyle';
 
 class LoginView extends Component {
@@ -17,7 +18,14 @@ class LoginView extends Component {
         return (
             <div style={CommonStyles.homeRightContainer}>
                 <div style={styles.container}>
-                    login
+                    <div style={styles.loginLayout}>
+                        <div style={styles.loginHeaderLayout}>
+
+                        </div>
+                        <div style={styles.loginContentLayout}>
+
+                        </div>
+                    </div>
                 </div>
             </div>
         );
@@ -26,8 +34,27 @@ class LoginView extends Component {
 
 const styles = {
     container: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: Constant.screenHeight - 40,
+    },
+    loginLayout: {
+        width: '60%',
+        height: 300,
+        border: '1px solid #888888',
+        borderRadius: 10,
+    },
+    loginHeaderLayout: {
 
     },
+    loginContentLayout: {
+
+    },
+    loginButton: {
+
+    }
 
 };
 
