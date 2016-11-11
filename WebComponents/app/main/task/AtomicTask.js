4dc3525def2e2216886479a316fd70b4a5a7a26f
MeePwn <maybewaityou@gmail.com>
2016-11-11 11:13:10 +0800

    first commit

--- WebComponents/app/main/task/AtomicTask.js
+++ WebComponents/app/main/task/AtomicTask.js
@@ -6,7 +6,12 @@ import Just from '../context/Just';
 
 const AtomicTask = {
 
-
+    function validateTellerTask(userID) {
+        return Just.promiseTask('padAdapter', 'validateTeller', [userID])
+            .then(function (result) {
+                return Just.promise(result.responseJSON.bsadata);
+            });
+    },
 
 };
 
