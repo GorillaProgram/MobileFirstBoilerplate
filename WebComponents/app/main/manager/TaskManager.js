d83004b69b5639324ced5ab250cdcbf1a0af3d07
MeePwn <maybewaityou@gmail.com>
2016-11-13 15:43:07 +0800

    first commit

--- WebComponents/app/main/manager/TaskManager.js
+++ WebComponents/app/main/manager/TaskManager.js
@@ -1,6 +1,9 @@
 /**
  * Created by MeePwn
  * https://github.com/maybewaityou
+ *
+ * description:
+ *      AtomicTask管理类, 用来组合AtomicTask, 形成交易顺序
  */
 import AtomicTask from '../task/AtomicTask';
 
