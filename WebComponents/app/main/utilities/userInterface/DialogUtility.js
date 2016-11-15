be89edaf42961ddbc8a0061a317a795b9090a462
MeePwn <maybewaityou@gmail.com>
2016-11-15 11:14:42 +0800

    first commit

--- WebComponents/app/main/utilities/userInterface/DialogUtility.js
+++ WebComponents/app/main/utilities/userInterface/DialogUtility.js
@@ -218,7 +218,7 @@ const DialogUtility = {
                 ICPositiveAction();
             }
             DialogUtility.dismissDialog();
-        })
+        });
     },
     /**
      * 刷指纹Dialog
