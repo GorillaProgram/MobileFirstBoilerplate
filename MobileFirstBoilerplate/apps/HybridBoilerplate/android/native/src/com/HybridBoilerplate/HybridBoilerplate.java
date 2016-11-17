584fc5890a83bc7b79d40710a1bac934e6ed2e37
MeePwn <maybewaityou@gmail.com>
2016-11-17 19:09:26 +0800

    first commit

--- MobileFirstBoilerplate/apps/HybridBoilerplate/android/native/src/com/HybridBoilerplate/HybridBoilerplate.java
+++ MobileFirstBoilerplate/apps/HybridBoilerplate/android/native/src/com/HybridBoilerplate/HybridBoilerplate.java
@@ -1,34 +1,33 @@
 package com.HybridBoilerplate;
 
+import org.apache.cordova.CordovaActivity;
+
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
 
-import org.apache.cordova.CordovaActivity;
-
 import com.worklight.androidgap.api.WL;
-import com.worklight.androidgap.api.WLInitWebFrameworkResult;
 import com.worklight.androidgap.api.WLInitWebFrameworkListener;
+import com.worklight.androidgap.api.WLInitWebFrameworkResult;
 
-public class HybridBoilerplate extends CordovaActivity implements WLInitWebFrameworkListener {
-	
+public class HybridBoilerplate extends CordovaActivity implements
+		WLInitWebFrameworkListener {
+
 	@Override
-	public void onCreate(Bundle savedInstanceState){
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		WL.createInstance(this);
-
 		WL.getInstance().showSplashScreen(this);
-
 		WL.getInstance().initializeWebFramework(getApplicationContext(), this);
-		
 	}
 
 	/**
-	 * The IBM MobileFirst Platform calls this method after its initialization is complete and web resources are ready to be used.
+	 * The IBM MobileFirst Platform calls this method after its initialization
+	 * is complete and web resources are ready to be used.
 	 */
- 	public void onInitWebFrameworkComplete(WLInitWebFrameworkResult result){
+	public void onInitWebFrameworkComplete(WLInitWebFrameworkResult result) {
 		if (result.getStatusCode() == WLInitWebFrameworkResult.SUCCESS) {
 			super.loadUrl(WL.getInstance().getMainHtmlFilePath());
 		} else {
@@ -36,14 +35,15 @@ public class HybridBoilerplate extends CordovaActivity implements WLInitWebFrame
 		}
 	}
 
-	private void handleWebFrameworkInitFailure(WLInitWebFrameworkResult result){
+	private void handleWebFrameworkInitFailure(WLInitWebFrameworkResult result) {
 		AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
-		alertDialogBuilder.setNegativeButton(R.string.close, new OnClickListener() {
-			@Override
-			public void onClick(DialogInterface dialog, int which){
-				finish();
-			}
-		});
+		alertDialogBuilder.setNegativeButton(R.string.close,
+				new OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						finish();
+					}
+				});
 
 		alertDialogBuilder.setTitle(R.string.error);
 		alertDialogBuilder.setMessage(result.getMessage());
