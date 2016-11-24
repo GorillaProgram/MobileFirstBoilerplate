7aca6fb17715c32eb5a376fbb95bb7d24dae70b8
MeePwn <maybewaityou@gmail.com>
2016-11-24 19:39:32 +0800

    first commit

--- WebComponents/app/pages/product/components/ProductDetailsView.js
+++ WebComponents/app/pages/product/components/ProductDetailsView.js
@@ -36,8 +36,7 @@ class ProductDetailsView extends Component {
 const styles = {
     container: {
         display: 'flex',
-        width: '100%',
-        height: '100%',
+        flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
     },
